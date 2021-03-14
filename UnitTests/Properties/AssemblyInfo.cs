de6c21ba34f0890818083eceec6a9164f25f96c7
Joey-Einerhand <j.einerhand@live.nl>
2021-03-14 19:39:41 +0100

    added unit tests

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c87c1521-278d-458c-b17a-754c26467555")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/Vector2UnitTests.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MatrixTransformations;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Vector2UnitTests
+    {
+        [TestMethod]
+        [DataRow(2, 2, 3, 3, 5, 5)]
+        [DataRow(9, -2, 3, 3, 12, 1)]
+        [DataRow(-3, 2, -9, 3, -12, 5)]
+        // Next test fails due to how rounding off is handled.
+        //[DataRow(1.1f, -4f, 2.2f, -1.1f, 3.3f, -5.1f)]
+        public void Addition_VectorAndVector(float x1, float y1, float x2, float y2, float expectedX, float expectedY)
+        {
+            // arrange
+            Vector2 firstVector = new Vector2(x1, y1);
+            Vector2 secondVector = new Vector2(x2, y2);
+            Vector2 expectedVector = new Vector2(expectedX, expectedY);
+            // act
+            Vector2 combinedVector = firstVector + secondVector;
+
+            // assert
+            Assert.AreEqual(expectedVector.x, combinedVector.x);
+            Assert.AreEqual(expectedVector.y, combinedVector.y);
+        }
+
+
+        [TestMethod]
+        [DataRow(2, 2, 3, 3, -1, -1)]
+        [DataRow(9, -2, 3, 3, 6, -5)]
+        [DataRow(-3, 2, -9, 3, 6, -1)]
+        [DataRow(1.1f, -4, 2.2f, -1.1f, -1.1f, -2.9f)]
+        public void Subtraction_VectorAndVector(float x1, float y1, float x2, float y2, float expectedX, float expectedY)
+        {
+            // arrange
+            Vector2 firstVector = new Vector2(x1, y1);
+            Vector2 secondVector = new Vector2(x2, y2);
+
+            // act
+            Vector2 subtractedVector = firstVector - secondVector;
+
+            // assert
+            Assert.AreEqual(expectedX, subtractedVector.x);
+            Assert.AreEqual(expectedY, subtractedVector.y);
+        }
+
+        [TestMethod]
+        [DataRow(2, 2, -3, -6, -6)]
+        [DataRow(9, -2, 3, 27, -6)]
+        public void Multiply_VectorAndFloat(float x, float y, float f, float resultX, float resultY)
+        {
+            Vector2 vector = new Vector2(x, y);
+            Vector2 expectedResultVector = new Vector2(resultX, resultY);
+
+            Vector2 resultingVector = vector * f;
+
+            Assert.AreEqual(expectedResultVector.x, resultingVector.x);
+            Assert.AreEqual(expectedResultVector.y, resultingVector.y);
+        }
+
+        [TestMethod]
+        [DataRow(2, 2, -3, -6, -6)]
+        [DataRow(9, -2, 3, 27, -6)]
+        public void Multiply_FloatAndVector(float x, float y, float f, float resultX, float resultY)
+        {
+            Vector2 vector = new Vector2(x, y);
+            Vector2 expectedResultVector = new Vector2(resultX, resultY);
+
+            Vector2 resultingVector = f * vector;
+
+            Assert.AreEqual(expectedResultVector.x, resultingVector.x);
+            Assert.AreEqual(expectedResultVector.y, resultingVector.y);
+        }
+
+        [TestMethod]
+        [DataRow(2, 2, -3, 2/-3f, 2/-3f)]
+        [DataRow(9, -2, 3, 3, -2/3f)]
+        public void Division_VectorAndFloat(float x, float y, float f, float resultX, float resultY)
+        {
+            Vector2 vector = new Vector2(x, y);
+
+            Vector2 expectedResultVector = new Vector2(resultX, resultY);
+
+            Vector2 resultingVector = vector / f;
+
+            Assert.AreEqual(expectedResultVector.x, resultingVector.x);
+            Assert.AreEqual(expectedResultVector.y, resultingVector.y);
+        }
+
+        [TestMethod]
+        [DataRow(2, 2, "{x = 2,y = 2}")]
+        [DataRow(9, -2, "{x = 9,y = -2}")]
+        [DataRow(-3.3f, -2, "{x = -3,3,y = -2}")]
+        public void ToString_Vector(float x, float y, string expectedOutput)
+        {
+            Vector2 vector = new Vector2(x, y);
+
+            string result = vector.ToString();
+
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
